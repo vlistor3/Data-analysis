8479936ee79b1992279bdbb4b2e5d7fe495891ee
Vlistor3 <gfmarques@protonmail.com>
2022-12-20 19:28:14 +0100

    Final script to have all the results in a single file. Way too long, next time do it by batches!

--- EVM/Optimism_analysis.py
+++ EVM/Optimism_analysis.py
@@ -12,39 +12,40 @@ Df = pd.DataFrame()
 # Parameters can be passed into SQL statements 
 # via native string interpolation
 while Day >= datetime.datetime(2022,9,1):
-    Daylastweek = Day - datetime.timedelta(hours = 6)
-    print(Daylastweek)
+    Daylastweek = Day - datetime.timedelta(hours = 12)
+    #print(Daylastweek)
     D  = f"{Day.year}-{Day.month}-{Day.day} {Day.hour}"
-    print(D)
+    #print(D)
     D_7 = f"{Daylastweek.year}-{Daylastweek.month}-{Daylastweek.day} {Daylastweek.hour}"
-    print(D_7)
-    print(f"The day is {Day}")
-    print(f"A week before is {Daylastweek}")
+    #print(D_7)
+    print(f"The querry is before {Day}")
+    print(f"The querry is after {Daylastweek}")
     sql = f"""
         SELECT
-            date_trunc('hour', block_timestamp) AS day,
-            tx_json:receipt:logs as JSON,
-            tx_hash
+            *,
+            date_trunc('hour', block_timestamp) as day
         FROM
-            optimism.core.fact_transactions
+            optimism.core.fact_event_logs
         WHERE
-            (day > '{D_7}' AND day <= '{D}')
-        AND CONTAINS(tx_json:receipt:logs:: string, '0x4200000000000000000000000000000000000042')
-         """
+            contract_address = '0x4200000000000000000000000000000000000042'
+        AND (day > '{D_7}' AND day <= '{D}')
+            """
 
     # Run the query against Flipside's query engine 
     # and await the results
     query_result_set = sdk.query(sql)
 
-    for record in query_result_set.records:
+    Df = Df.append(query_result_set.records)
+    """ for record in query_result_set.records:
         print(record)
-        Df = Df.append({'Day' : record['day'], 'JSON' : record['json'], 'tx_hash' : record['tx_hash']}, ignore_index = True)
+        Df = Df.append(record, ignore_index = True) """
     print(f"{Df} \n")
     Day = Daylastweek
 
-started_at = query_result_set.run_stats.started_at
-ended_at = query_result_set.run_stats.ended_at
-elapsed_seconds = query_result_set.run_stats.elapsed_seconds
-record_count = query_result_set.run_stats.record_count
+    started_at = query_result_set.run_stats.started_at
+    ended_at = query_result_set.run_stats.ended_at
+    elapsed_seconds = query_result_set.run_stats.elapsed_seconds
+    record_count = query_result_set.run_stats.record_count
 
-print(f"This query took ${elapsed_seconds} seconds to run and returned {record_count} records from the database.")
+    print(f"This query took ${elapsed_seconds} seconds to run and returned {record_count} records from the database.")
+Df.to_pickle(f"C:/Users/vlist/.vscode/GitHub/Data-analysis/EVMExport_Df.pkl")
